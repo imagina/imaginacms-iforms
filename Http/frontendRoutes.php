f283640437907e806ea243da3fdede9219f59db1
Marcos Rativa <marcosr@imaginacolombia.com>
2018-10-19 14:36:08 -0500

    delete multi-language route

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -5,10 +5,8 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 $router->group(['prefix' => 'iforms'], function (Router $router) {
 
-    $locale = LaravelLocalization::setLocale() ?: App::getLocale();
-
     $router->post('/lead', [
-        'as' => $locale.'.iforms.lead',
+        'as' => 'iforms.lead',
         'uses' => 'PublicController@store',
         //'middleware' => config('asgard.blog.config.middleware'),
     ]);
