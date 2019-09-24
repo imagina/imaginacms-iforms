160aecb0362b73d1dc15687f9a4d191a8aacacf9
Yeison Tapia <yftapial@ut.edu.co>
2019-09-24 17:15:59 -0500

    + Add Api controllers

---
+++ Http/Controllers/Api/FieldApiController.php
@@ -0,0 +1,157 @@
+<?php
+
+
+namespace Modules\Iform\Http\Controllers\Api;
+
+// Requests & Response
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Modules\Iform\Http\Requests\CreateFieldRequest as CreateRequest;
+use Modules\Iform\Http\Requests\UpdateFieldRequest as UpdateRequest;
+
+// Base Api
+use Modules\Ihelpers\Http\Controllers\Api\BaseApiController;
+
+// Transformers
+use Modules\Iform\Transformers\FieldTransformer as Transformer;
+
+// Repositories
+use Modules\Iform\Repositories\FieldRepository;
+
+class FieldApiController extends BaseApiController
+{
+  private $resource;
+
+  public function __construct(FieldRepository $resource)
+  {
+    $this->resource = $resource;
+  }
+
+  /**
+   * GET ITEMS
+   *
+   * @return mixed
+   */
+  public function index(Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $data = $this->resource->getItemsBy($params);
+      //Response
+      $response = ["data" => Transformer::collection($data)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($data)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * GET A ITEM
+   *
+   * @param $criteria
+   * @return mixed
+   */
+  public function show($criteria, Request $request)
+  {
+    try {
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
+      //Request to Repository
+      $data = $this->resource->getItem($criteria, $params);
+      //Break if no found item
+      if (!$data) throw new Exception('Item not found', 204);
+      //Response
+      $response = ["data" => new Transformer($data)];
+      //If request pagination add meta-page
+      $params->page ? $response["meta"] = ["page" => $this->pageTransformer($data)] : false;
+    } catch (\Exception $e) {
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * CREATE A ITEM
+   *
+   * @param Request $request
+   * @return mixed
+   */
+  public function create(Request $request)
+  {
+    \DB::beginTransaction();
+    try {
+      $data = $request->input('attributes') ?? [];//Get data
+      //Validate Request
+      $this->validateRequestApi(new CreateRequest($data));
+      //Create item
+      $newData = $this->resource->create($data);
+      //Response
+      $response = ["data" => new Transformer($newData)];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   * @param  Request $request
+   * @return Response
+   */
+  public function update($criteria, Request $request)
+  {
+    \DB::beginTransaction();
+    try {
+      $params = $this->getParamsRequest($request);
+
+      $data = $request->input('attributes');
+      //Validate Request
+      $this->validateRequestApi(new UpdateRequest($data));
+      //Update data
+      $newData = $this->resource->updateBy($criteria, $data, $params);
+      //Response
+      $response = ['data' => $newData];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   * @return Response
+   */
+  public function delete($criteria, Request $request)
+  {
+    \DB::beginTransaction();
+    try {
+      //Get params
+      $params = $this->getParamsRequest($request);
+      //Delete data
+      $this->resource->deleteBy($criteria, $params);
+      //Response
+      $response = ['data' => ''];
+      \DB::commit(); //Commit to Data Base
+    } catch (\Exception $e) {
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
+    }
+    return response()->json($response, $status ?? 200);
+  }
+}
