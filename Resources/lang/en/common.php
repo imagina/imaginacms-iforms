90c534384540b0e02d68ce1cd59526c6cbdb8c89
Slim Arias Sanhez <slimarias@github.com>
2020-10-07 10:29:06 -0500

    FIX: common english field types translations

--- Resources/lang/en/common.php
+++ Resources/lang/en/common.php
@@ -1,19 +1,67 @@
 <?php
 
 return [
-    'iforms'=>'Forms',
-    'form_user' => 'Restrict to User',
-    'title' => 'Títle',
-    'to'=>'To',
-    'created_at' => 'Creation date',
-    'fields'=>'fields',
-    'translate'=>'Translate Rows',
-    'emails'=>'E-mail',
-    'recaptcha'=>'Enable google reCAPTCHA',
-    'no'=>'No',
-    'yes'=>'Yes',
-    'api'=>'API',
-    'email_from'=>'Email From',
-    'forms_not_found'=>'Forms not found',
-    'captcha_required'=> 'Captcha is required'
+  'iforms' => 'Forms',
+
+  'button' => [
+    "read more" => "Read More"
+  ],
+  'table' => [
+  ],
+  'form' => [
+  ],
+  'messages' => [
+    'title is required' => 'The title is required',
+    'title min 2' => 'Title must contain at least two characters',
+    'description is required' => 'The Description is required',
+    'description min 2' => 'The description must contain at least two characters',
+    'summary is required' => 'The summary is required',
+    'summary min 2' => 'The summary must contain at least two characters',
+  ],
+  'validation' => [
+  ],
+  'types' => [
+    'text' => 'Text',
+    'number' => 'Number',
+    'textarea' => 'Text Area',
+    'email' => 'E-mail',
+    'select' => 'Select',
+    'selectmultiple' => 'Multi select',
+    'checkbox' => 'Check',
+    'radio' => 'Radio',
+    'city' => 'City',
+    'department' => 'Departament',
+    'country' => 'Country',
+    'location' => 'Location',
+    'phone' => 'Phone',
+    'date' => 'Date',
+  ],
+
+  'status_text' => 'Estatus',
+
+  'image' => 'Image',
+
+  'categories' => 'Categories',
+
+  'title' => 'Títle',
+  'slug' => 'Slug',
+  'description' => 'Description',
+  'meta_title' => 'Meta Title',
+  'meta_description' => 'Meta Description',
+
+  'date' => 'Date and Time',
+  'optional' => '(Optional)',
+
+  'summary' => 'Sumary',
+  'content' => 'Content',
+  'Posted on' => 'Posted on',
+  'author' => 'Author',
+  'by' => 'by',
+  'default_category' => 'Default Category',
+
+  'admin_notes' => 'Admin`s notes',
+  'created_at' => 'Creation date',
+  'parent' => 'Parent',
+
+
 ];
