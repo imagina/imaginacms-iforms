82f4d613b7faf37bd2a56766b2197eef5a22d3f3
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-04-29 10:10:25 -0500

    +REVERT

--- Resources/lang/es/fields.php
+++ Resources/lang/es/fields.php
@@ -17,7 +17,7 @@ return [
     ],
     'form' => [
         'email' => [
-            'name' => 'correo',
+            'name' => 'email',
             'label' => 'Tu E-mail...',
             'placeholder' => 'Tu E-mail...',
             'description' => 'Tu E-mail...',
