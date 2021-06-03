403b812c5148ed24ff198b8f1d0b420bab8cb8e1
Juan Escalante <jcec007@gmail.com>
2021-06-02 19:45:54 -0500

    +Add: added order by by default in the fields relation in the Block entity +Add: exportable config

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,34 @@
 <?php
 
 return [
-    'name' => 'Iforms'
+  'name' => 'Iforms',
+  /*
+|--------------------------------------------------------------------------
+| Define all the exportable available
+|--------------------------------------------------------------------------
+*/
+  'exportable' => [
+    "leads" => [
+      'moduleName' => "Iforms",
+      'exportName' => "LeadsPerFormExport",
+      'fileName' => "Leads",
+      'exportFields' => [
+        "formId" => [
+          "value" => null,
+          "type" => 'select',
+          "required" => true,
+          "props" => [
+            "label" => 'Formulario*',
+          ],
+          "loadOptions" => [
+            "apiRoute" => 'apiRoutes.qform.forms',
+            "select" => ["label" => 'title', "id" => 'id'],
+          ]
+        ]
+      ]
+      //'repositoryName' => "UserApiRepository",
+      //'fields' => ['id', 'first_name', 'last_name', 'email', 'last_login', 'created_at', 'updated_at'],
+      //'headings' => ['id', 'Nombre', 'Apellido', 'Email', 'Fecha Ultima Sesión', 'Fecha de Creación', 'Fecha Ultima Actualización']
+    ]
+  ]
 ];
