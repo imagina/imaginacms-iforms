908ab390617b7ea035d7164ef5df614fd534edc6
Juan Escalante <jcec007@gmail.com>
2021-09-20 19:34:42 -0500

    +Update: improve Lead PDF view

--- Resources/views/pdf/leadItem.blade.php
+++ Resources/views/pdf/leadItem.blade.php
@@ -1,24 +1,44 @@
 @php
     $lead=$items->first();
     $form=$lead->form;
+    $blocks=$form->blocks;
 
 @endphp
-<h1 style="font-size: 22px">Formulario: {{ $form->title }}</h1>
-<h3>Registro #{{$lead->id}}</h3>
+<h1 style="font-size: 16px">Formulario: {{ $form->title }}</h1>
+<h5 style="margin: 0 10px 10px 0;">Registro #{{$lead->id}}</h5>
 @if(isset($lead->assignedTo->id))
-<p style="font-size: 16px">
+<p style="font-size: 10px">
     Asignado a: {{ $lead->assignedTo->present()->fullname }}
 </p>
 @endif
 
-<table style="width: 100%;border-collapse: collapse;">
-    <tbody>
-@foreach($lead->values as $index => $field)
-    <tr>
-    <th style="background-color: #eee;">{{ $index }}</th> <td>{{ $field }}</td>
-    </tr>
+@foreach($blocks as $block)
+    <hr  style="color: #eee; margin: 5px">
+    @if(!empty($block->title))
+    <h5 style="margin: 0 10px 10px 0;">{{$block->title}}</h5>
+    @endif
+    @if(!empty($block->description))
+    <p>{{$block->description}}</p>
+    @endif
+    <table style="border: 1px solid #ebebeb;
+    padding: 20px;
+    width: 80%;
+    border-radius: 1em;
+    overflow: hidden;
+    text-align: center;
+    margin: 0 auto;
+    margin-bottom:20px">
+        <tbody>
+        @foreach($block->fields as $blockField)
+            <tr>
+                <th style="font-size: 12px; color:#333333; text-align: left">{{ $blockField->label }}</th>
+                <td style="font-size: 12px; text-align: left">{{ $lead->values[$blockField->name] ?? '' }}</td>
+            </tr>
+        @endforeach
+        
+        </tbody>
+    </table>
+    
 @endforeach
 
-    </tbody>
-</table>
 <br>
