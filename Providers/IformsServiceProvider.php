6254765379c093d892f040be112cd058de43e947
Juan Escalante <jcec007@gmail.com>
2021-10-04 18:34:38 -0500

    +Update: remove settings and permissions from the published config

--- Providers/IformsServiceProvider.php
+++ Providers/IformsServiceProvider.php
@@ -15,116 +15,119 @@ use Illuminate\Support\Facades\Config;
 
 class IformsServiceProvider extends ServiceProvider
 {
-    use CanPublishConfiguration;
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerBindings();
-        $this->app['events']->listen(BuildingSidebar::class, RegisterIformsSidebar::class);
-
-        $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
-            $event->load('forms', Arr::dot(trans('iforms::forms')));
-            $event->load('fields', Arr::dot(trans('iforms::fields')));
-            $event->load('leads', Arr::dot(trans('iforms::leads')));
-        });
-
-    }
-
-    public function boot()
-    {
-        $this->publishConfig('iforms', 'permissions');
-        $this->publishConfig('iforms', 'config');
-        $this->publishConfig('iforms', 'settings');
-        $this->publishConfig('iforms', 'settings-fields');
-        $this->publishConfig('iforms', 'crud-fields');
-        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-
-        $this->registerComponents();
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('Iforms');
-    }
-
-    private function registerBindings()
-    {
-        $this->app->bind(
-            'Modules\Iforms\Repositories\FormRepository',
-            function () {
-                $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentFormRepository(new \Modules\Iforms\Entities\Form());
-
-                if (! config('app.cache')) {
-                    return $repository;
-                }
-
-                return new \Modules\Iforms\Repositories\Cache\CacheFormDecorator($repository);
-            }
-        );
-        $this->app->bind(
-            'Modules\Iforms\Repositories\FieldRepository',
-            function () {
-                $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentFieldRepository(new \Modules\Iforms\Entities\Field());
-
-                if (! config('app.cache')) {
-                    return $repository;
-                }
-
-                return new \Modules\Iforms\Repositories\Cache\CacheFieldDecorator($repository);
-            }
-        );
-        $this->app->bind(
-            'Modules\Iforms\Repositories\LeadRepository',
-            function () {
-                $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentLeadRepository(new \Modules\Iforms\Entities\Lead());
-
-                if (! config('app.cache')) {
-                    return $repository;
-                }
-
-                return new \Modules\Iforms\Repositories\Cache\CacheLeadDecorator($repository);
-            }
-        );
-        $this->app->bind(
-            'Modules\Iforms\Repositories\BlockRepository',
-            function () {
-                $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentBlockRepository(new \Modules\Iforms\Entities\Block());
-
-                if (! config('app.cache')) {
-                    return $repository;
-                }
-
-                return new \Modules\Iforms\Repositories\Cache\CacheBlockDecorator($repository);
-            }
-        );
+  use CanPublishConfiguration;
+  
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
+  
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->registerBindings();
+    $this->app['events']->listen(BuildingSidebar::class, RegisterIformsSidebar::class);
+    
+    $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
+      $event->load('forms', Arr::dot(trans('iforms::forms')));
+      $event->load('fields', Arr::dot(trans('iforms::fields')));
+      $event->load('leads', Arr::dot(trans('iforms::leads')));
+    });
+    
+  }
+  
+  public function boot()
+  {
+    
+    $this->publishConfig('iforms', 'config');
+    $this->mergeConfigFrom($this->getModuleConfigFilePath('iforms', 'settings'), "asgard.iforms.settings");
+    $this->mergeConfigFrom($this->getModuleConfigFilePath('iforms', 'settings-fields'), "asgard.iforms.settings-fields");
+    $this->mergeConfigFrom($this->getModuleConfigFilePath('iforms', 'permissions'), "asgard.iforms.permissions");
+    $this->publishConfig('iforms', 'crud-fields');
+    $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    
+    $this->registerComponents();
+  }
+  
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return array('Iforms');
+  }
+  
+  private function registerBindings()
+  {
+    $this->app->bind(
+      'Modules\Iforms\Repositories\FormRepository',
+      function () {
+        $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentFormRepository(new \Modules\Iforms\Entities\Form());
+        
+        if (!config('app.cache')) {
+          return $repository;
+        }
+        
+        return new \Modules\Iforms\Repositories\Cache\CacheFormDecorator($repository);
+      }
+    );
+    $this->app->bind(
+      'Modules\Iforms\Repositories\FieldRepository',
+      function () {
+        $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentFieldRepository(new \Modules\Iforms\Entities\Field());
+        
+        if (!config('app.cache')) {
+          return $repository;
+        }
+        
+        return new \Modules\Iforms\Repositories\Cache\CacheFieldDecorator($repository);
+      }
+    );
+    $this->app->bind(
+      'Modules\Iforms\Repositories\LeadRepository',
+      function () {
+        $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentLeadRepository(new \Modules\Iforms\Entities\Lead());
+        
+        if (!config('app.cache')) {
+          return $repository;
+        }
+        
+        return new \Modules\Iforms\Repositories\Cache\CacheLeadDecorator($repository);
+      }
+    );
+    $this->app->bind(
+      'Modules\Iforms\Repositories\BlockRepository',
+      function () {
+        $repository = new \Modules\Iforms\Repositories\Eloquent\EloquentBlockRepository(new \Modules\Iforms\Entities\Block());
+        
+        if (!config('app.cache')) {
+          return $repository;
+        }
+        
+        return new \Modules\Iforms\Repositories\Cache\CacheBlockDecorator($repository);
+      }
+    );
 // add bindings
-
-
-        $this->app->bind('Modules\Iforms\Presenters\FormPresenter');
-
-    }
-
-    /**
-     * Register Blade components
-     */
-
-    private function registerComponents(){
-        Blade::componentNamespace("Modules\Iforms\View\Components", 'iforms');
-    }
+    
+    
+    $this->app->bind('Modules\Iforms\Presenters\FormPresenter');
+    
+  }
+  
+  /**
+   * Register Blade components
+   */
+  
+  private function registerComponents()
+  {
+    Blade::componentNamespace("Modules\Iforms\View\Components", 'iforms');
+  }
 }
