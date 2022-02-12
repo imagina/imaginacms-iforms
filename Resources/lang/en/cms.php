5676871ee4dcd9be961f820d8598f6cf472499fe
Michael <msolano@imaginacolombia.com>
2022-02-11 19:09:01 -0500

    +Update: cms Translation files

--- Resources/lang/en/cms.php
+++ Resources/lang/en/cms.php
@@ -1,33 +1,42 @@
 <?php
-return[
-  "newForm" => 'New Form',
-  "updateForm" => 'Update Form',
-  "newField" => 'New Field',
-  "updateField" => 'Update Field',
-  "newBlock" => 'new Block',
-  "updateBlock" => 'Update Block',
-  "deleteBlock" => 'Delete Block',
-  "replyTo"  => 'Field to reply to',
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Forms",
+    "adminForm" => "List Forms",
+    "adminField" => "List Fields",
+    "adminLead" => "List Lead",
+    "designForm" => "Design Form"
+  ],
+  "newForm" => "New Form",
+  "updateForm" => "Update Form",
+  "newField" => "New Field",
+  "updateField" => "Update Field",
+  "newBlock" => "new Block",
+  "updateBlock" => "Update Block",
+  "deleteBlock" => "Delete Block",
+  "replyTo" => "Field to reply to",
   "form" => [
-    "label" => 'Label',
-    "placeholder" => 'Placeholder',
-    "form" => 'Form',
-    "order" => 'Order',
-    "suffix" => 'Suffix',
-    "urlTermsAndConditions" => 'URL Terms and Conditions',
-    "value" => 'Value',
+    "label" => "Label",
+    "placeholder" => "Placeholder",
+    "form" => "Form",
+    "order" => "Order",
+    "suffix" => "Suffix",
+    "urlTermsAndConditions" => "URL Terms and Conditions",
+    "value" => "Value",
     "columns" => [
-      "three" => '25%',
-      "four" => '33%',
-      "six" => '50%',
-      "eight" => '66%',
-      "nine" => '75%',
-      "full" => 'Full Width',
+      "three" => "25%",
+      "four" => "33%",
+      "six" => "50%",
+      "eight" => "66%",
+      "nine" => "75%",
+      "full" => "Full Width"
     ],
-    "successText"  => 'Success text',
-    "submitText"  => 'Submit text',
+    "successText" => "Success text",
+    "submitText" => "Submit text"
   ],
   "message" => [
-    "helpToOptionsField" => 'Type the option and press enter...'
+    "helpToOptionsField" => "Type the option and press enter..."
   ]
 ];
+
