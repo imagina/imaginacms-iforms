c93d753a04a68fb948dd727116ec2f9efa14d8ce
msolano <msolano@imaginacolombia.com>
2022-03-01 19:22:55 -0500

    +Update: CMSSidebar config

--- Config/cmsSidebar.php
+++ Config/cmsSidebar.php
@@ -6,8 +6,8 @@ return [
       "title" => "iforms.cms.sidebar.adminGroup",
       "icon" => "fas fa-clipboard-list",
       "children" => [
-        "iform_cms_admin_forms",
-        "iform_cms_admin_leads",
+        "iforms_cms_admin_forms",
+        "iforms_cms_admin_leads",
       ]
     ]
   ],
