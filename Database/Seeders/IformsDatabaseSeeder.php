c7b68b688aeda7f3b6796aabb8bb82b41b0e1aae
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:17:11 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IformsDatabaseSeeder.php
+++ Database/Seeders/IformsDatabaseSeeder.php
@@ -4,20 +4,21 @@ namespace Modules\Iforms\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IformsDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(IformsModuleTableSeeder::class);
-        $this->call(BlockTableSeeder::class);
-        $this->call(ContactFormTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Iforms\Database\Seeders",
+      "seeds" => ["IformsModuleTableSeeder", "BlockTableSeeder", "ContactFormTableSeeder"]
+    ]);
+  }
 }
