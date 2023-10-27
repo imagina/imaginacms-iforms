d662047a0f626e44d61b2e776db75f7c4bca3d26
Cristian Marin <marin101998@gmail.com>
2023-10-27 16:24:58 -0500

    +add | rules in create request fields and forms

--- Http/Requests/CreateFieldRequest.php
+++ Http/Requests/CreateFieldRequest.php
@@ -3,37 +3,40 @@
 namespace Modules\Iforms\Http\Requests;
 
 use Modules\Core\Internationalisation\BaseFormRequest;
-use Modules\Media\Validators\AvailableExtensionsRule;
+use Modules\Iforms\Rules\OnlyOneNestedChildRule;
+use Modules\Iforms\Rules\CheckFieldParentIdRule;
 
 
 class CreateFieldRequest extends BaseFormRequest
 {
-    public function rules()
-    {
-        return [
-          "options.availableExtensions" => [
-            new AvailableExtensionsRule()
-          ]
-        ];
-    }
+  public function rules()
+  {
+    $fieldId = $this->get('parent_id');
+    return [
+      'form_id' => ['required', new CheckFieldParentIdRule($fieldId)],
+      "parent_id" => [
+        new OnlyOneNestedChildRule('iforms__fields')
+      ]
+    ];
+  }
 
-    public function translationRules()
-    {
-        return [];
-    }
+  public function translationRules()
+  {
+    return [];
+  }
 
-    public function authorize()
-    {
-        return true;
-    }
+  public function authorize()
+  {
+    return true;
+  }
 
-    public function messages()
-    {
-        return [];
-    }
+  public function messages()
+  {
+    return [];
+  }
 
-    public function translationMessages()
-    {
-        return [];
-    }
+  public function translationMessages()
+  {
+    return [];
+  }
 }
--- Http/Requests/CreateFormRequest.php
+++ Http/Requests/CreateFormRequest.php
@@ -3,31 +3,36 @@
 namespace Modules\Iforms\Http\Requests;
 
 use Modules\Core\Internationalisation\BaseFormRequest;
+use Modules\Iforms\Rules\OnlyOneNestedChildRule;
 
 class CreateFormRequest extends BaseFormRequest
 {
-    public function rules()
-    {
-        return [];
-    }
+  public function rules()
+  {
+    return [
+      "parent_id" => [
+        new OnlyOneNestedChildRule('iforms__forms')
+      ]
+    ];
+  }
 
-    public function translationRules()
-    {
-        return [];
-    }
+  public function translationRules()
+  {
+    return [];
+  }
 
-    public function authorize()
-    {
-        return true;
-    }
+  public function authorize()
+  {
+    return true;
+  }
 
-    public function messages()
-    {
-        return [];
-    }
+  public function messages()
+  {
+    return [];
+  }
 
-    public function translationMessages()
-    {
-        return [];
-    }
+  public function translationMessages()
+  {
+    return [];
+  }
 }
