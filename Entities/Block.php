4518877221664a6173cd795c037adc57cd05ff62
NicolasMB <nmontealegre@imaginacolombia.com>
2024-05-27 12:38:26 -0500

    Fixed: issue about double decode

--- Entities/Block.php
+++ Entities/Block.php
@@ -47,10 +47,12 @@ class Block extends Model
 
     public function getOptionsAttribute($value)
     {
-        try {
-            return json_decode(json_decode($value));
-        } catch (\Exception $e) {
-            return json_decode($value);
+        $response = json_decode($value);
+
+        if(is_string($response)) {
+          $response = json_decode($response);
         }
+
+        return $response;
     }
 }
