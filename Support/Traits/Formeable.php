eec3dce0474fed8280849ee57e3fe43d40663a52
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2024-10-10 15:19:45 -0500

    + fix | v10 10-10-24 fixed sync forms (#94)

--- Support/Traits/Formeable.php
+++ Support/Traits/Formeable.php
@@ -6,47 +6,48 @@ use Modules\Iforms\Entities\Form;
 
 trait Formeable
 {
-    /**
-     * Boot trait method
-     */
-    public static function bootFormeable()
-    {
-        //Listen event after create model
-        static::createdWithBindings(function ($model) {
-            //Sync schedules
-            $model->syncForms($model->getEventBindings('createdWithBindings'));
-        });
-        //Listen event after update model
-        static::updatedWithBindings(function ($model) {
-            //Sync Schedules
-            $model->syncForms($model->getEventBindings('updatedWithBindings'));
-        });
-        //Listen event after delete model
-        static::deleted(function ($model) {
-            // ...Do something
-        });
-    }
+  /**
+   * Boot trait method
+   */
+  public static function bootFormeable()
+  {
+    //Listen event after create model
+    static::createdWithBindings(function ($model) {
+      //Sync schedules
+      $model->syncForms($model->getEventBindings('createdWithBindings'));
+    });
+    //Listen event after update model
+    static::updatedWithBindings(function ($model) {
+      //Sync Schedules
+      $model->syncForms($model->getEventBindings('updatedWithBindings'));
+    });
+    //Listen event after delete model
+    static::deleted(function ($model) {
+      // ...Do something
+    });
+  }
 
-    //Sync Forms
-    public function syncForms($params)
-    {
-        //Instance form ID
-        $formsId = isset($params['data']['form_id']) && $params['data']['form_id'] ?
-          [$params['data']['form_id']] : [];
+  //Sync Forms
+  public function syncForms($params)
+  {
+    //Instance form ID
+    if (isset($params['data']['form_id'])) {
+      $formsId = $params['data']['form_id'] ? [$params['data']['form_id']] : [];
 
-        //Sync Forms
-        $this->forms()->sync($formsId);
+      //Sync Forms
+      $this->forms()->sync($formsId);
     }
+  }
 
-    /**
-     * Make the Productable morph relation
-     */
-    public function forms()
-    {
-        return $this->morphToMany(Form::class, 'formeable', 'iforms__formeable')
-          ->withPivot('formeable_type')
-          ->withTimestamps();
-    }
+  /**
+   * Make the Productable morph relation
+   */
+  public function forms()
+  {
+    return $this->morphToMany(Form::class, 'formeable', 'iforms__formeable')
+      ->withPivot('formeable_type')
+      ->withTimestamps();
+  }
 
   public function form()
   {
