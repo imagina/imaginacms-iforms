10dc40a0af21ab857e968f0197ea9d25e186d100
josuebedoya348 <145929750+josuebedoya@users.noreply.github.com>
2025-03-03 11:23:13 -0500

    Add terms and Conditions (#99)
    * fix: Backend | Access to fieldOptions with concatenation array
    
    * +  Backend | add validation to forms
    
    * + Backend | Add Terms and conditions from layouts forms

--- Resources/views/frontend/components/form/layouts/form-layout-2/index.blade.php
+++ Resources/views/frontend/components/form/layouts/form-layout-2/index.blade.php
@@ -9,6 +9,19 @@
 
     @include('iforms::frontend.components.form.layouts.form-layout-2.fields')
 
+    <!--Validate field terms and conditions-->
+    @if(isset($form->options->urlTermsAndConditions))
+      <!--Content Terms and Conditions -->
+      <div id="contentTermsAndConditions" class="col-12 position-relative mb-3">
+        <div id="CheckFormTermsAndConditions" class="pl-4">
+          <input type="checkbox" class="form-check-input" required="" id="TermsAndConditions">
+          <label class="form-check-label h6" for="TermsAndConditions">
+            {!! trans('iforms::forms.form.termsAndConditions', ['urlTermsAndConditions' =>$form->options->urlTermsAndConditions]) !!}
+          </label>
+        </div>
+      </div>
+    @endif
+
     <x-isite::captcha :formId="$formId"/>
     <div class="w-100 {{$buttonAlign}}">
         <button type="submit"
--- Resources/views/frontend/components/form/layouts/form-layout-3/index.blade.php
+++ Resources/views/frontend/components/form/layouts/form-layout-3/index.blade.php
@@ -7,6 +7,20 @@
     <input type="hidden" name="form_id" value="{{$form->id}}" required="">
 
     @include('iforms::frontend.components.form.layouts.form-layout-3.fields')
+
+    <!--Validate field terms and conditions-->
+    @if(isset($form->options->urlTermsAndConditions))
+      <!--Content Terms and Conditions -->
+      <div id="contentTermsAndConditions" class="col-12 position-relative mb-3">
+        <div id="CheckFormTermsAndConditions" class="pl-4">
+          <input type="checkbox" class="form-check-input" required="" id="TermsAndConditions">
+          <label class="form-check-label h6" for="TermsAndConditions">
+            {!! trans('iforms::forms.form.termsAndConditions', ['urlTermsAndConditions' =>$form->options->urlTermsAndConditions]) !!}
+          </label>
+        </div>
+      </div>
+    @endif
+
     <div class="col-sm-12">
       <x-isite::captcha :formId="$formId"/>
     </div>
