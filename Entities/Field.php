6fd37591a53edfab1f4c9a0e8e6a609337fe5eb8
msolanorenuity <msolano@contractor.renuityhome.com>
2025-05-02 10:04:16 -0500

    Fix return value for empty field options in Field entity
    Ensure `getFieldOptionsAttribute` returns an empty array when `field_options` has no value. This prevents potential errors and maintains compatibility with legacy implementations.

--- Entities/Field.php
+++ Entities/Field.php
@@ -153,7 +153,7 @@ class Field extends CrudModel
   public function getFieldOptionsAttribute()
   {
     $fieldOptions = $this->fields->where('name', 'field_options')->first();
-    if ($fieldOptions) return $fieldOptions->value;
+    if ($fieldOptions) return $fieldOptions->value ?? [];
     //getting the options from the selectable attribute for old sites created with the Iform before Dec, 2021
     return $this->options['fieldOptions'] ?? json_decode($this->selectable) ?? [];
   }
