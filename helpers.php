22448fbc678105ac2556f21a2cd4d7e18b1fb4a1
Cristian Marin <marin101998@gmail.com>
2025-05-10 11:18:31 -0500

    + fix | v10 10-05-25 new helper to find fileds by name

--- helpers.php
+++ helpers.php
@@ -19,3 +19,17 @@ if (! function_exists('iform')) {
         return $view->render();
     }
 }
+
+if (!function_exists('iforms_findFieldByName')) {
+  function iforms_findFieldByName(array $names): array
+  {
+    $fieldRepository = app('Modules\Iforms\Repositories\FieldRepository');
+    $params = [
+      'filter' => [
+        'name' => $names,
+      ],
+    ];
+    $fields = $fieldRepository->getItemsBy(json_decode(json_encode($params)));
+    return $fields->keyBy('name')->all(); // Devuelve: name => fieldObject
+  }
+}
