919b86a74f7bab3f48ead299c00cf42c4c88c41b
Cristian Marin <54483189+CristianMarin19@users.noreply.github.com>
2025-05-15 11:46:14 -0500

    + fix | v10 15-05-25 add function updateOrders in cache and interfaces (#107)
    * + fix | v10 15-04-25 Fix quote form
    
    * + fix | v10 21-04-25 fix form id in create leads
    
    * + fix | v10 10-05-25 new helper to find fileds by name
    
    * + fix | v10 15-05-25 add function updateOrders in cache and interfaces

--- Repositories/Cache/CacheFieldDecorator.php
+++ Repositories/Cache/CacheFieldDecorator.php
@@ -7,11 +7,19 @@ use Modules\Core\Icrud\Repositories\Cache\BaseCacheCrudDecorator;
 
 class CacheFieldDecorator extends BaseCacheCrudDecorator implements FieldRepository
 {
-    public function __construct(FieldRepository $field)
-    {
-        parent::__construct();
-        $this->entityName = 'iforms.fields';
-        $this->tags = ['iforms.blocks','iforms.forms'];
-        $this->repository = $field;
-    }
+  public function __construct(FieldRepository $field)
+  {
+    parent::__construct();
+    $this->entityName = 'iforms.fields';
+    $this->tags = ['iforms.blocks', 'iforms.forms'];
+    $this->repository = $field;
+  }
+
+  public function updateOrders($data)
+  {
+    return $this->remember(function () use ($data) {
+      return $this->repository->updateOrders($data);
+    });
+  }
+
 }
